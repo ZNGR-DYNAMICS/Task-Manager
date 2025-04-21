0df8e50e9324341d6e469a1614f46e803719db0c
n-zngr <deltagaming.ch.nik@gmail.com>
2025-04-21 22:15:52 +0200

    feat(main): add react router
    - install react-router dependency
    - add `/task` as route

--- app/main.tsx
+++ app/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx'
+import TaskView from './pages/TaskView.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/task" element={<TaskView />} />
+            </Routes>
+        </BrowserRouter>
     </StrictMode>,
 )
