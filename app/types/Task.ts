20af7269c8d6c7ba1d180cd072be99a16cde2fc4
n-zngr <deltagaming.ch.nik@gmail.com>
2025-04-23 12:05:55 +0200

    feat(types): add type for dnd, update statuses

--- app/types/Task.ts
+++ app/types/Task.ts
@@ -1,10 +1,11 @@
 export interface Task {
     id: string;
+    type: 'Task';
     title: string;
     description?: string;
     assignee?: string;
     dueDate?: string;
     taskSize?: 'Small' | 'Medium' | 'Large';
     files?: string[];
-    status: 'Backlog' | 'InProgress' | 'Committed';
+    status: 'Backlog' | 'InProgress' | 'OnHold' | 'Committed' | 'Cancelled' | 'Released';
 }
