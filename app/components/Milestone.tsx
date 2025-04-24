4401cef70297cf72855decd637d418072ced3582
n-zngr <deltagaming.ch.nik@gmail.com>
2025-04-24 22:04:16 +0200

    style(tasks): improve styling

--- app/components/Milestone.tsx
+++ app/components/Milestone.tsx
@@ -1,7 +1,7 @@
 
 const Milestone: React.FC = () => {
     return (
-        <div className="flex bg-white/5 border border-white5 rounded-lg text-white">
+        <div className="w-full bg-white-5 border border-white-10 rounded-lg flex-grow overflow-y-auto">
             <div className="flex gap-16">
                 <h1>Milestone</h1>
                 <p>1 / 3 / 4</p>
