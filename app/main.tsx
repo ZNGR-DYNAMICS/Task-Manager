c6da8aac16e408f912bad0f892fdfc9ed726028d
n-zngr <deltagaming.ch.nik@gmail.com>
2025-04-24 12:05:45 +0200

    feat(main): wrap DND around routes

--- app/main.tsx
+++ app/main.tsx
@@ -6,15 +6,19 @@ import '../fonts/poppins.css';
 import App from './App.tsx'
 import TaskView from './pages/TaskView.tsx';
 import DND from './DnDTest/DND.tsx';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<App />} />
-                <Route path="/task" element={<TaskView />} />
-                <Route path="/dnd" element={<DND />} />
-            </Routes>
+            <DndProvider backend={HTML5Backend}>
+                <Routes>
+                    <Route path="/" element={<App />} />
+                    <Route path="/task" element={<TaskView />} />
+                    <Route path="/dnd" element={<DND />} />
+                </Routes>
+            </DndProvider>
         </BrowserRouter>
     </StrictMode>,
 )
