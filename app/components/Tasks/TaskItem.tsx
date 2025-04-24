fcd88b49a30b7c08596442090d85c145f37aa41b
n-zngr <deltagaming.ch.nik@gmail.com>
2025-04-24 12:06:10 +0200

    feat(tasks): update task to allow for dnd

--- app/components/Tasks/TaskItem.tsx
+++ app/components/Tasks/TaskItem.tsx
@@ -1,28 +1,33 @@
-import { useState } from 'react';
-import { Task } from '../../types/Task';
+import { useRef } from 'react';
+import { useDrag } from 'react-dnd';
+import type { Task } from '../../types/Task';
 
 interface TaskItemProps {
     task: Task;
 }
 
-const TaskItem: React.FC<TaskItemProps> = ({ ...task }) => {
-    const [selected, setSelected] = useState(false);
+const TaskItem: React.FC<TaskItemProps> = ({ task }) => {
+    const ref = useRef<HTMLDivElement>(null);
+    const [{ isDragging }, drag] = useDrag(() => ({
+        type: "Task",
+        item: { ...task },
+        collect: (monitor) => ({
+            isDragging: !!monitor.isDragging(),
+        }),
+    }), [task]);
 
-    function handleSelect() {
-        setSelected(true);
+    drag(ref);
 
-        if (selected === true) {
-            setSelected(false);
-        }
-    }
 
     return (
-        <button
-            className={`text-left w-full hover:bg-white-10 ${selected ? 'bg-white-10' : ''} rounded-md p-2 text-sm font-medium transition-colors duration-200 cursor-pointer`}
-            onClick={handleSelect}
+        <div
+            ref={ref}
+            className={`text-left w-full hover:bg-white-10 rounded-md p-2 text-sm font-medium transition-colors duration-200 cursor-move ${
+                isDragging ? 'bg-white-10' : ''
+            }`}
         >
             {task.title}
-        </button>
+        </div>
     )
 }
 
