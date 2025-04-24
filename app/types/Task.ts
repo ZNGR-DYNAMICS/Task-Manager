4228a00bc261fc44acfdfc1d1727a1de9cb97454
n-zngr <deltagaming.ch.nik@gmail.com>
2025-04-24 18:50:46 +0200

    feat(types)!: add dateCreated, update order

--- app/types/Task.ts
+++ app/types/Task.ts
@@ -1,11 +1,12 @@
 export interface Task {
     id: string;
-    type: 'Task';
     title: string;
+    status: 'Backlog' | 'InProgress' | 'OnHold' | 'Committed' | 'Cancelled' | 'Released';
+    dateCreated: string;
     description?: string;
     assignee?: string;
-    dueDate?: string;
+    dateDue?: string;
     taskSize?: 'Small' | 'Medium' | 'Large';
     files?: string[];
-    status: 'Backlog' | 'InProgress' | 'OnHold' | 'Committed' | 'Cancelled' | 'Released';
+    type: 'Task';
 }
